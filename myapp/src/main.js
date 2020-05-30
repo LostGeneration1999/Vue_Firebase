304fb7f2c662c170597f2cdf2aa60603ac51aa1c
LostGeneration1999 <masaya4426saganosen@gmail.com>
2020-05-30 22:57:30 +0900

    修正

--- myapp/src/main.js
+++ myapp/src/main.js
@@ -14,14 +14,7 @@ Vue.config.productionTip = false
 
 // Firebase設定
 let firebaseConfig = {
-  apiKey: "AIzaSyCxrSkpsTwCm5CPT6bMCy_EZ0kw6smJloQ",
-  authDomain: "mindmap-app-d9302.firebaseapp.com",
-  databaseURL: "https://mindmap-app-d9302.firebaseio.com",
-  projectId: "mindmap-app-d9302",
-  storageBucket: "mindmap-app-d9302.appspot.com",
- // messagingSenderId: "xxxxxxxxxx",
- // appId: "xxxxxxxxxx"
- };
+}
 
 firebase.initializeApp(firebaseConfig);
 export const db = firebase.firestore()
